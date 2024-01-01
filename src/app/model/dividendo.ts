ae13318c47cc22c6378b085ce5982013a58d2180
alexpereira2014 <alexpereira2004@gmail.com>
2024-01-01 17:50:09 -0300

    Dividendo - Classe model

---
+++ src/app/model/dividendo.ts
@@ -0,0 +1,12 @@
+
+export class Dividendo {
+  id: string;
+  dataRecebimento: string;
+  dataRecebimentoInicio: string;
+  dataRecebimentoFinal: string;
+  tipo: string;
+  quantidade: string;
+  dividendo: string;
+  valorTotal: string;
+  ativoCodigo: string;
+}
