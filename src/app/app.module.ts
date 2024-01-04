aae46458ae2d90d540e147bd1255a144cc3dabc6
alexpereira2014 <alexpereira2004@gmail.com>
2024-01-03 22:22:19 -0300

    HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AtivoModule } from "./view/ativo/ativo.module";
 import { AtivoRoutingModule } from "./view/ativo/ativo-routing.module";
 import { DividendoModule } from "./view/dividendo/dividendo.module";
 import { DividendoRoutingModule } from "./view/dividendo/dividendo-routing.module";
+import { HttpClientModule } from "@angular/common/http";
 
 
 @NgModule({
@@ -15,6 +16,7 @@ import { DividendoRoutingModule } from "./view/dividendo/dividendo-routing.modul
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule,
     AtivoModule,
     AtivoRoutingModule,
