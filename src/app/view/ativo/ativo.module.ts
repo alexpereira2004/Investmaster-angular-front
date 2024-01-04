5e9ea259f8746aa2ac5c565297e7efe1985d8232
alexpereira2014 <alexpereira2004@gmail.com>
2024-01-03 22:23:54 -0300

    Ativo - Componente de listagem

--- src/app/view/ativo/ativo.module.ts
+++ src/app/view/ativo/ativo.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AtivoRoutingModule } from './ativo-routing.module';
 import { AtivoCadastroComponent } from "./ativo-cadastro/ativo-cadastro.component";
+import { AtivoListagemComponent } from './ativo-listagem/ativo-listagem.component';
 
 
 @NgModule({
   declarations: [
-    AtivoCadastroComponent
+    AtivoCadastroComponent,
+    AtivoListagemComponent
   ],
   imports: [
     CommonModule,
