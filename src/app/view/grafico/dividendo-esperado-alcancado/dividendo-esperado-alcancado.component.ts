7e09b2ca43bb92b4621d73cb2b19f83beab516a3
alexpereira2014 <alexpereira2004@gmail.com>
2024-07-10 19:29:31 -0300

    Primeiro grafico funcionando https://github.com/znjs/chart-js-samples/blob/master/src/app/line-chart/line-chart.component.ts

--- src/app/view/grafico/dividendo-esperado-alcancado/dividendo-esperado-alcancado.component.ts
+++ src/app/view/grafico/dividendo-esperado-alcancado/dividendo-esperado-alcancado.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Chart from 'chart.js/auto';
 
 @Component({
   selector: 'app-dividendo-esperado-alcancado',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dividendo-esperado-alcancado.component.css']
 })
 export class DividendoEsperadoAlcancadoComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.createChart();
   }
 
+  public lineChart: any;
+
+  createChart() {
+    this.lineChart = new Chart('lineChart', {
+      type: 'line',
+      data: {
+        labels: ['jan', 'feb', 'mar', 'april', 'may', 'june', 'july', 'aug'],
+        datasets: [
+          {
+            label: 'My First Dataset',
+            data: [65, 59, 80, 81, 56, 55, 40],
+            fill: false,
+            borderColor: 'rgb(75, 192, 192)',
+            tension: 0.1,
+          },
+        ],
+      },
+    });
+  }
 }
