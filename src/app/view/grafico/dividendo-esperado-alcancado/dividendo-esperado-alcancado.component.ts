08062e4aabf9a47d1f4aacb43cfa7d4d5d0bf3b3
alexpereira2014 <alexpereira2004@gmail.com>
2024-07-17 19:08:32 -0300

    Grafico linha com dois datasets

--- src/app/view/grafico/dividendo-esperado-alcancado/dividendo-esperado-alcancado.component.ts
+++ src/app/view/grafico/dividendo-esperado-alcancado/dividendo-esperado-alcancado.component.ts
@@ -17,23 +17,53 @@ export class DividendoEsperadoAlcancadoComponent implements OnInit {
 
   createChart() {
     this.lineChart = new Chart('lineChart', {
-      type: 'line',
+      type: 'bar',
       data: {
         labels: ['jan', 'feb', 'mar', 'april', 'may', 'june', 'july', 'aug'],
         datasets: [
           {
-            label: 'My First Dataset',
+            label: 'Barra 1',
             data: [65, 59, 80, 81, 56, 55, 40],
-            fill: false,
-            borderColor: 'rgb(75, 192, 192)'
+            borderColor: 'rgb(201,191,129)',
+            backgroundColor: 'rgb(201,191,129)',
+            order: 2
           },
           {
-            label: 'My Second Dataset',
-            data: [50, 51, 54, 59, 65, 69, 70],
-            fill: false,
-            borderColor: 'rgb(200, 0, 0)'
+            label: 'Barra 2',
+            data: [50, 40, 30, 20, 10, 5, 1],
+            borderColor: 'rgb(206,125,125)',
+            backgroundColor: 'rgb(200, 150, 150)',
+            order: 3
           },
+          {
+            label: 'Linha 1',
+            data:  [50, 51, 54, 59, 65, 69, 70],
+            borderColor: 'rgb(255,202,0)',
+            backgroundColor: 'rgb(255,195,0)',
+            type: 'line',
+            order: 0
+          },
+          {
+            label: 'Linha 2',
+            data:  [40, 55, 40, 60, 30, 70, 20],
+            borderColor: 'rgb(250,0,0)',
+            backgroundColor: 'rgb(255,0,0)',
+            type: 'line',
+            order: 1
+          }
         ],
+      },
+      options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'top',
+          },
+          title: {
+            display: true,
+            text: 'Dividendos Esperados x Alcançados'
+          }
+        }
       },
     });
   }
