53e63d2d7063814321c3e4e87219b158504d6694
alexpereira2014 <alexpereira2004@gmail.com>
2024-10-03 19:08:48 -0300

    Ajustes menores

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,13 @@
 export const environment = {
   production: true,
   portalApi: {
-    baseUrl: 'http://localhost:12001/',
+    baseUrl: 'http://localhost:8501',
     recurso: {
       ativo: "v2/ativo",
       ativoId: "v2/ativo/{id}",
+      ativoPaginado: "v2/ativo/listagem-paginado",
+      projecao: "v2/projecao/listagem-paginado",
+      projecaoAnos: "v2/projecao/anos"
     }
   }
 };
