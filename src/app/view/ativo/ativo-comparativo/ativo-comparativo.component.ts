e16cdd67cc072c7ee3a19b921e46de5544b31dac
alexpereira2014 <alexpereira2004@gmail.com>
2024-12-07 19:15:21 -0300

    #5 Base inicial da pagina

---
+++ src/app/view/ativo/ativo-comparativo/ativo-comparativo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ativo-comparativo',
+  standalone: true,
+  imports: [],
+  templateUrl: './ativo-comparativo.component.html',
+  styleUrl: './ativo-comparativo.component.css'
+})
+export class AtivoComparativoComponent {
+
+}
