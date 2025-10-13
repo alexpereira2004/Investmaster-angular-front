574dc35d3bcae096b7d5a7df60e55bc104523cdb
alexpereira2014 <alexpereira2004@gmail.com>
2025-10-12 22:33:31 -0300

    Atualizacao para Angular 19

--- src/app/view/ativo/ativo-monitor/ativo-monitor.component.ts
+++ src/app/view/ativo/ativo-monitor/ativo-monitor.component.ts
@@ -2,8 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-ativo-monitor',
-  standalone: true,
-  imports: [],
   templateUrl: './ativo-monitor.component.html',
   styleUrl: './ativo-monitor.component.css'
 })
