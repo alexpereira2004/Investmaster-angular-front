e1870b9c98ff5d163be4c3a12c3e5120ef0eb615
alexpereira2014 <alexpereira2004@gmail.com>
2025-11-02 23:14:55 -0300

    #40 Requesta para pesquisar na tabela monitor

--- src/app/service/monitor/monitor.service.ts
+++ src/app/service/monitor/monitor.service.ts
@@ -1,30 +1,54 @@
 import { Injectable } from '@angular/core';
 import { Observable, of } from "rxjs";
-import { MonitorListagem } from "../../model/monitor-listagem";
+import { Monitor } from "../../model/monitor";
+import { environment } from "../../../environments/environment";
+import { PageSpring } from "../../model/page-spring";
+import { HttpClient } from "@angular/common/http";
+import { ApiRequestsService } from "../util/api-requests.service";
+import { MonitorFilter } from "../../model/filter/monitor-filter";
 
 @Injectable({
   providedIn: 'root'
 })
 export class MonitorService {
 
-  private ativosMonitorados: MonitorListagem[] = [
-    { codigo: 'BBAS3', nome: 'Banco do Brasil', situacao: 'situacao-1' },
-    { codigo: 'BBSE3', nome: 'BB Seguridade', situacao: 'situacao-2'},
-    { codigo: 'VALE3', nome: 'Vale', situacao: 'situacao-3'},
-    { codigo: 'PETR4', nome: 'Petrobrás', situacao: 'situacao-4'},
-    { codigo: 'BRAP4', nome: 'Bradespar', situacao: 'situacao-5'},
-    { codigo: 'VLID3', nome: 'Vallid', situacao: 'situacao-5'},
-    { codigo: 'AGRO3', nome: 'MFII11', situacao: 'situacao-7'},
-    { codigo: 'MTRE3', nome: 'MFII11', situacao: 'situacao-7'},
-    { codigo: 'DCRA11', nome: 'DCRA11', situacao: 'situacao-8'},
-    { codigo: 'GARE11', nome: 'GARE11', situacao: 'situacao-9'},
-    { codigo: 'MXRF11', nome: 'FII MXRF11', situacao: 'situacao-10' },
-    { codigo: 'CXSE3', nome: 'Caixa Seguridade', situacao: 'situacao-6'}
+  private baseUrl;
+  private recursos;
+
+  private ativosMonitorados: Monitor[] = [
+    { codigoAtivo: 'BBAS3', nome: 'Banco do Brasil', situacao: 'situacao-1' },
+    { codigoAtivo: 'BBSE3', nome: 'BB Seguridade', situacao: 'situacao-2'},
+    { codigoAtivo: 'VALE3', nome: 'Vale', situacao: 'situacao-3'},
+    { codigoAtivo: 'PETR4', nome: 'Petrobrás', situacao: 'situacao-4'},
+    { codigoAtivo: 'BRAP4', nome: 'Bradespar', situacao: 'situacao-5'},
+    { codigoAtivo: 'VLID3', nome: 'Vallid', situacao: 'situacao-5'},
+    { codigoAtivo: 'AGRO3', nome: 'MFII11', situacao: 'situacao-7'},
+    { codigoAtivo: 'MTRE3', nome: 'MFII11', situacao: 'situacao-7'},
+    { codigoAtivo: 'DCRA11', nome: 'DCRA11', situacao: 'situacao-8'},
+    { codigoAtivo: 'GARE11', nome: 'GARE11', situacao: 'situacao-9'},
+    { codigoAtivo: 'MXRF11', nome: 'FII MXRF11', situacao: 'situacao-10' },
+    { codigoAtivo: 'VLID3', nome: 'VLID3', situacao: 'situacao-10' },
+    { codigoAtivo: 'CXSE3', nome: 'Caixa Seguridade', situacao: 'situacao-6'}
   ];
 
-  constructor() { }
+  constructor(
+    private httpClient: HttpClient,
+    private util: ApiRequestsService
+  ) {
+    this.baseUrl = environment.portalApi.baseUrl;
+    this.recursos = environment.portalApi.recurso;
+  }
 
-  getAtivosMonitorados(): Observable<MonitorListagem[]> {
+  getAtivosMonitorados(): Observable<Monitor[]> {
     return of(this.ativosMonitorados);
+  }
+
+  pesquisarComFiltroPaginado(filter: MonitorFilter) {
+    const url = environment.portalApi.baseUrl
+      + environment.portalApi.recurso.monitorListagemPaginado;
+
+    const params = this.util.buildParams(filter);
+
+    return this.httpClient.get<PageSpring<Monitor>>(url, {params: params});
   }
 }
