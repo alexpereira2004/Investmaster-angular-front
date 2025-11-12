86bb1762e155ebc4d061d31f31d141c45b778086
alexpereira2014 <alexpereira2004@gmail.com>
2025-11-12 00:17:58 -0300

    #20 Criacao da página de Cadastro/Edicao de Regras de monitoramento

--- src/app/view/monitor/monitor-cadastro/monitor-cadastro.component.ts
+++ src/app/view/monitor/monitor-cadastro/monitor-cadastro.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { AtivoService } from "../../../service/ativo/ativo.service";
+import { Ativo } from "../../../model/ativo";
 
 @Component({
   standalone: false,
@@ -6,6 +9,34 @@ import { Component } from '@angular/core';
   templateUrl: './monitor-cadastro.component.html',
   styleUrl: './monitor-cadastro.component.css'
 })
-export class MonitorCadastroComponent {
+export class MonitorCadastroComponent implements OnInit {
 
+  public FRMregraMonitoramento!: FormGroup;
+  ativoLista!: Ativo[];
+
+  constructor(private formBuilder: FormBuilder,
+              private ativoService: AtivoService
+  ) {
+    this.FRMregraMonitoramento = this.formBuilder.group({
+      ativo: [null, Validators.required],
+
+    });
+  }
+
+  ngOnInit(): void {
+    this.ativoService.pesquisarPaginado(0, 2000).subscribe({
+      next: result => {
+        this.ativoLista = result.content;
+      },
+      error: (error) => {
+        console.log(error);
+      },
+      complete: () => {}
+    });
+  }
+
+
+  onSubmit() {
+
+  }
 }
