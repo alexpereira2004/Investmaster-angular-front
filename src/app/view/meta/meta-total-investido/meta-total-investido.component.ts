591e59b5c3ec7b9d54c914c1b85394bd36e4a5d2
alexpereira2014 <alexpereira2004@gmail.com>
2026-06-14 17:05:54 -0300

    #47 Criado o primriro componente de Meta

---
+++ src/app/view/meta/meta-total-investido/meta-total-investido.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: false,
+  selector: 'app-meta-total-investido',
+  templateUrl: './meta-total-investido.component.html',
+  styleUrl: './meta-total-investido.component.css'
+})
+export class MetaTotalInvestidoComponent {
+
+}
