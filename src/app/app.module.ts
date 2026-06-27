41daf55339b80299c7c6654b99e4cb5582b92a36
alexpereira2014 <alexpereira2004@gmail.com>
2026-06-27 14:56:32 -0300

    Força o app inteiro a usar o padrão PT-BR

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -50,7 +50,9 @@ registerLocaleData(localePt, 'pt-BR');
       echarts: () => import('echarts')
     })
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt-BR' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
